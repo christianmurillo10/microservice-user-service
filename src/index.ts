21259f6e6f91fc169ea0a89db3ab977df39dab3d
Christian Murillo <christianoroceomurillo@gmail.com>
2025-04-12 19:21:21 +0800

    Updates

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ import serverConfig from "./config/server.config";
 import KafkaServer from "./events";
 
 (async () => {
+  // Kafka server
+  await KafkaServer.listen();
+  
   // Express server
   const app = new App(Number(serverConfig.port));
   await app.start();
-
-  // Kafka server
-  KafkaServer.listen();
 })();
