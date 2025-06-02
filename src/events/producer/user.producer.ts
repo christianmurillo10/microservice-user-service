3b6dc8c3ea119f96db10c47c775781edf531873d
Christian Murillo <christianoroceomurillo@gmail.com>
2025-06-02 21:10:17 +0800

    Implemented producer for user delete

--- src/events/producer/user.producer.ts
+++ src/events/producer/user.producer.ts
@@ -1,7 +1,7 @@
 import { IHeaders } from "kafkajs";
 import KafkaService from "../../services/kafka.service";
 import kafkaConfig from "../../config/kafka.config";
-import { EVENT_USER, EVENT_USER_CREATED, EVENT_USER_UPDATED } from "../../shared/constants/events.constant";
+import { EVENT_USER, EVENT_USER_CREATED, EVENT_USER_DELETED, EVENT_USER_UPDATED } from "../../shared/constants/events.constant";
 import UsersModel from "../../models/users.model";
 import { EventMessageData } from "../../shared/types/common.type";
 
@@ -24,6 +24,12 @@ export default class UserKafkaProducer {
   publishUserUpdated = async (data: EventMessageData<UsersModel>, headers?: IHeaders): Promise<void> => {
     await this.kafkaService.connectProducer();
     await this.kafkaService.initializeProducer(EVENT_USER, EVENT_USER_UPDATED, data, headers);
+    await this.kafkaService.disconnectProducer();
+  };
+
+  publishUserDeleted = async (data: EventMessageData<UsersModel>, headers?: IHeaders): Promise<void> => {
+    await this.kafkaService.connectProducer();
+    await this.kafkaService.initializeProducer(EVENT_USER, EVENT_USER_DELETED, data, headers);
     await this.kafkaService.disconnectProducer();
   };
 };
