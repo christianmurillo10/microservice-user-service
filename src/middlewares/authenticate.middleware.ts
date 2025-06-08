43456e6b13c12c6a4a53759324f6ebd92ccf40d0
Christian Murillo <christianoroceomurillo@gmail.com>
2025-06-08 19:23:55 +0800

    Fixed auth user details issue

--- src/middlewares/authenticate.middleware.ts
+++ src/middlewares/authenticate.middleware.ts
@@ -83,11 +83,11 @@ const authenticate = async (
         throw new NotFoundException([MESSAGE_INVALID_API_KEY]);
       }
 
-      req.auth = usersRecord;
       req.businesses = businessesRecord;
       req.body.business_id = businessesRecord.id;
     }
 
+    req.auth = usersRecord;
     next();
   })
   .catch(err => next(err));
