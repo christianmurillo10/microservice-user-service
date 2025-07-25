7a94fb166ccbe217c4799fe64e3314dc0331a7f5
Christian Murillo <christianoroceomurillo@gmail.com>
2025-07-26 01:04:08 +0800

    Update prisma seeders

--- prisma/seeders/index.ts
+++ prisma/seeders/index.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import roles from "./roles";
 import users from "./users";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  await roles.down();
-  await roles.up();
   await users.down();
   await users.up();
 };
